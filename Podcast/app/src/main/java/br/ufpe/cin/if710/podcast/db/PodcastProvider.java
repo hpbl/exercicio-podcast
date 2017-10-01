f4649372874a23247152d1af2d44cb14f648e058
Hilton Pintor <hpbl@cin.ufpe.br>
2017-10-01 00:31:00 -0300

    Suportando inserção no banco

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/db/PodcastProvider.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/db/PodcastProvider.java
@@ -1,17 +1,35 @@
 package br.ufpe.cin.if710.podcast.db;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteAbortException;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
 
 public class PodcastProvider extends ContentProvider {
+    // instância do helper que vai ser utilizada nos métodos do provider
+    private PodcastDBHelper mPodcastDBHelper;
+
     public PodcastProvider() {
     }
 
     @Override
+    public boolean onCreate() {
+        // COMPLETED: Implement this to initialize your content provider on startup.
+        Context context = this.getContext();
+        this.mPodcastDBHelper = PodcastDBHelper.getInstance(context);
+        return true;
+    }
+
+    @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
+        //  Implement this to handle requests to delete one or more rows.
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
@@ -24,14 +42,23 @@ public class PodcastProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+        // COMPLETED: Implement this to handle requests to insert a new row.
+        final SQLiteDatabase db = this.mPodcastDBHelper.getWritableDatabase();
 
-    @Override
-    public boolean onCreate() {
-        // TODO: Implement this to initialize your content provider on startup.
-        return false;
+        Uri returnUri;
+        Long id = db.insert(PodcastProviderContract.EPISODE_TABLE, null, values);
+
+        if (id > 0) {
+            //inserção feita com sucesso
+            returnUri = ContentUris.withAppendedId(PodcastProviderContract.EPISODE_LIST_URI, id);
+            Log.d("PodcastProvider", returnUri.toString());
+        } else {
+            throw new android.database.SQLException("falha na inserção em: " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return returnUri;
     }
 
     @Override
