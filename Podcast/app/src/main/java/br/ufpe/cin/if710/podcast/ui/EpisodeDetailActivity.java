13c03566b3b1490859d5525e23cdf5fd06f613fb
Hilton Pintor <hpbl@cin.ufpe.br>
2017-10-01 18:29:21 -0300

    Navegando para tela de detalhes de forma modal

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
@@ -2,16 +2,28 @@ package br.ufpe.cin.if710.podcast.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import br.ufpe.cin.if710.podcast.R;
+import br.ufpe.cin.if710.podcast.ui.adapter.XmlFeedAdapter;
 
 public class EpisodeDetailActivity extends Activity {
+    private TextView mTitleTV;
+    private TextView mDescriptionTV;
+    private TextView mPubDateTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episode_detail);
 
-        //TODO preencher com informações do episódio clicado na lista...
+        //COMPLETED preencher com informações do episódio clicado na lista...
+        this.mTitleTV = findViewById(R.id.title_tv);
+        this.mDescriptionTV = findViewById(R.id.description_tv);
+        this.mPubDateTV = findViewById(R.id.pubDate_tv);
+
+        this.mTitleTV.setText(this.getIntent().getExtras().getString(XmlFeedAdapter.TITLE_EXTRA));
+        this.mDescriptionTV.setText(this.getIntent().getExtras().getString(XmlFeedAdapter.DESCRIPTION_EXTRA));
+        this.mPubDateTV.setText(this.getIntent().getExtras().getString(XmlFeedAdapter.PUBDATE_EXTRA));
     }
 }
--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/adapter/XmlFeedAdapter.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/adapter/XmlFeedAdapter.java
@@ -1,16 +1,29 @@
 package br.ufpe.cin.if710.podcast.ui.adapter;
 
 import java.util.List;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import br.ufpe.cin.if710.podcast.R;
 import br.ufpe.cin.if710.podcast.domain.ItemFeed;
+import br.ufpe.cin.if710.podcast.ui.EpisodeDetailActivity;
 
 public class XmlFeedAdapter extends ArrayAdapter<ItemFeed> {
+
+    // Constantes para o intent
+    public static final String TITLE_EXTRA = "Title";
+    public static final String PUBDATE_EXTRA = "PubDate";
+    public static final String DESCRIPTION_EXTRA = "Description";
+    public static final String DOWNLOAD_LINK_EXTRA = "DownloadLink";
 
     int linkResource;
 
@@ -63,8 +76,32 @@ public class XmlFeedAdapter extends ArrayAdapter<ItemFeed> {
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
-        holder.item_title.setText(getItem(position).getTitle());
-        holder.item_date.setText(getItem(position).getPubDate());
+        // item atual
+        final ItemFeed item = getItem(position);
+        holder.item_title.setText(item.getTitle());
+        holder.item_date.setText(item.getPubDate());
+
+        // ouvindo clicks (tem que ser aqui pois tem um botão na lista)
+        // https://issuetracker.google.com/issues/36908602
+        holder.item_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // criando intent para trocar de tela
+                Context context = getContext();
+                Intent detailIntent = new Intent(context, EpisodeDetailActivity.class);
+
+                // passando dados pela intent
+                detailIntent.putExtra(TITLE_EXTRA, item.getTitle());
+                detailIntent.putExtra(PUBDATE_EXTRA, item.getPubDate());
+                detailIntent.putExtra(DESCRIPTION_EXTRA, item.getDescription());
+                detailIntent.putExtra(DOWNLOAD_LINK_EXTRA, item.getDownloadLink());
+
+                // chamando trasição de tela
+                context.startActivity(detailIntent);
+            }
+        });
+
         return convertView;
     }
+
 }
