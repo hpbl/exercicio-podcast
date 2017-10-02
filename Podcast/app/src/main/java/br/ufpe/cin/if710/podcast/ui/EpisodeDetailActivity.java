ec17d85923b42369c1b6c286b0e922eaaf6b373d
Hilton Pintor <hpbl@cin.ufpe.br>
2017-10-01 21:59:34 -0300

    Adicionando navegação de volta

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/EpisodeDetailActivity.java
@@ -2,6 +2,8 @@ package br.ufpe.cin.if710.podcast.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import br.ufpe.cin.if710.podcast.R;
@@ -17,6 +19,9 @@ public class EpisodeDetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episode_detail);
 
+        // Mostrando botão de voltar (up)
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
         //COMPLETED preencher com informações do episódio clicado na lista...
         this.mTitleTV = findViewById(R.id.title_tv);
         this.mDescriptionTV = findViewById(R.id.description_tv);
@@ -25,5 +30,16 @@ public class EpisodeDetailActivity extends Activity {
         this.mTitleTV.setText(this.getIntent().getExtras().getString(XmlFeedAdapter.TITLE_EXTRA));
         this.mDescriptionTV.setText(this.getIntent().getExtras().getString(XmlFeedAdapter.DESCRIPTION_EXTRA));
         this.mPubDateTV.setText(this.getIntent().getExtras().getString(XmlFeedAdapter.PUBDATE_EXTRA));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // botão de voltar
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
