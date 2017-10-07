3f6c2ad0c52c6307f6bf1581c740ea339c49c726
Hilton Pintor <hpbl@cin.ufpe.br>
2017-10-07 12:13:02 -0300

    Lendo dados sempre do banco

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/MainActivity.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/ui/MainActivity.java
@@ -2,16 +2,17 @@ package br.ufpe.cin.if710.podcast.ui;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -28,12 +29,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.ufpe.cin.if710.podcast.R;
-import br.ufpe.cin.if710.podcast.db.PodcastDBHelper;
-import br.ufpe.cin.if710.podcast.db.PodcastProvider;
 import br.ufpe.cin.if710.podcast.db.PodcastProviderContract;
 import br.ufpe.cin.if710.podcast.domain.ItemFeed;
 import br.ufpe.cin.if710.podcast.domain.XmlFeedParser;
-import br.ufpe.cin.if710.podcast.ui.adapter.XmlFeedAdapter;
+import br.ufpe.cin.if710.podcast.ui.adapter.PodcastItemAdapter;
 
 public class MainActivity extends Activity {
 
@@ -41,16 +40,29 @@ public class MainActivity extends Activity {
     private final String RSS_FEED = "http://leopoldomt.com/if710/fronteirasdaciencia.xml";
     //TODO teste com outros links de podcast
 
-    private ListView items;
+    private ListView itemsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        items = (ListView) findViewById(R.id.items);
+        itemsListView = (ListView) findViewById(R.id.items);
 
         Stetho.initializeWithDefaults(this);
+    }
+
+    // função para conferir se usuário tem conexão com a a internet
+    private boolean hasNetworkConnection() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        return isConnected;
     }
 
     @Override
@@ -77,20 +89,90 @@ public class MainActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-        new DownloadXmlTask().execute(RSS_FEED);
+
+        /* conferindo se o usuário tem conexão com a internet
+           para saber se deve ser feito o parse do XML */
+        if (this.hasNetworkConnection()) {
+            new DownloadXmlTask().execute(RSS_FEED);
+            Log.d("MAINACTIVITY", "COM INTERNET");
+        } else {
+            Log.d("MAINACTIVITY", "SEM INTERNET");
+            new DataBaseTask().execute();
+        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        XmlFeedAdapter adapter = (XmlFeedAdapter) items.getAdapter();
-        adapter.clear();
+        PodcastItemAdapter adapter = (PodcastItemAdapter) itemsListView.getAdapter();
+
+        // adapter vai ser null quando o usuário abrir o app pela primeira vez sem internet
+        if (adapter != null) {
+            adapter.clear();
+        }
+    }
+
+    // AssyncTask para pegar podcasts do banco de dados
+    private class DataBaseTask extends  AsyncTask<Void, Void, Cursor> {
+        @Override
+        protected void onPreExecute() {
+            // indicativo visual que o carregamento está sendo do banco
+            Toast.makeText(getApplicationContext(), "consultando banco", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... voids) {
+
+            // fazendo consulta (query) do banco
+            Cursor cursor = getContentResolver()
+                    .query(PodcastProviderContract.EPISODE_LIST_URI,
+                            null,
+                            null,
+                            null,
+                            null);
+
+            return cursor;
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            // caso não tenha nada no banco
+            if ((cursor == null) || (cursor.getCount() == 0)) {
+                // informe ao usuário que ele precisa de internet
+                Toast.makeText(getApplicationContext(), "Conecte-se à internet", Toast.LENGTH_LONG).show();
+            // caso tenha conteúdo no banco
+            } else {
+                Toast.makeText(getApplicationContext(), "terminando...", Toast.LENGTH_SHORT).show();
+
+                List<ItemFeed> items = new ArrayList<>();
+
+                while (cursor.moveToNext()) {
+                    String title = cursor.getString(cursor.getColumnIndex(PodcastProviderContract.EPISODE_TITLE));
+                    String link = cursor.getString(cursor.getColumnIndex(PodcastProviderContract.EPISODE_LINK));
+                    String date = cursor.getString(cursor.getColumnIndex(PodcastProviderContract.EPISODE_DATE));
+                    String description = cursor.getString(cursor.getColumnIndex(PodcastProviderContract.EPISODE_DESC));
+                    String downloadLink = cursor.getString(cursor.getColumnIndex(PodcastProviderContract.EPISODE_DOWNLOAD_LINK));
+
+                    ItemFeed item = new ItemFeed(title, link, date, description, downloadLink);
+                    items.add(item);
+                }
+
+                //Adapter Personalizado
+                PodcastItemAdapter adapter = new PodcastItemAdapter(getApplicationContext(),
+                                                                    R.layout.itemlista,
+                                                                    items);
+
+                //atualizar o list view
+                itemsListView.setAdapter(adapter);
+                itemsListView.setTextFilterEnabled(true);
+            }
+        }
     }
 
     private class DownloadXmlTask extends AsyncTask<String, Void, List<ItemFeed>> {
         @Override
         protected void onPreExecute() {
-            Toast.makeText(getApplicationContext(), "iniciando...", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "lendo XML", Toast.LENGTH_SHORT).show();
         }
 
         @Override
@@ -124,19 +206,14 @@ public class MainActivity extends Activity {
             } catch (XmlPullParserException e) {
                 e.printStackTrace();
             }
+
             return itemList;
         }
 
         @Override
         protected void onPostExecute(List<ItemFeed> feed) {
-            Toast.makeText(getApplicationContext(), "terminando...", Toast.LENGTH_SHORT).show();
-
-            //Adapter Personalizado
-            XmlFeedAdapter adapter = new XmlFeedAdapter(getApplicationContext(), R.layout.itemlista, feed);
-
-            //atualizar o list view
-            items.setAdapter(adapter);
-            items.setTextFilterEnabled(true);
+            // uma vez atualizado o conteúdo do banco, use o mesmo para povoar a listVew
+            new DataBaseTask().execute();
         }
     }
 
